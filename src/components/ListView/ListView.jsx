c37f83016edd05723f5483bf856c2515dff94916
Mateo Álvarez <alvarez.fing@gmail.com>
2021-06-27 22:49:21 -0300

    feat(usereducer): create a use reducer for todo list

--- src/components/ListView/ListView.jsx
+++ src/components/ListView/ListView.jsx
@@ -1,26 +1,57 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import { useForm } from '../../hooks/useForm'
+import uniqid from 'uniqid'
 
 const ListView = () => {
-  const initialState = {
-    task: '',
-    important: false
-  }
 
-  const { state, handleChange, reset } = useForm( initialState )
+  const { state, handleChange, reset } = useForm({ task: '', important: false })
   const { task, important } = state
 
-  const [listTask, setListTask] = useState([])
+  const taskReducer = (state, action) => {
+    switch (action.type) {
+      case 'add':
+        return [
+          ...state,
+          action.payload
+        ]
 
-  const handleSubmit = e => {
+      case 'changeImportant':
+        return state.map( task => task.id === action.payload
+          ? { ...task, important: !task.important}
+          : task
+        )
+      
+      case 'delete':
+        return state.filter(task => task.id !== action.payload)
+
+      case 'complete':
+          return state.map( task => task.id === action.payload
+            ? { ...task, isCompleted: !task.isCompleted }  
+            : state
+          )
+    
+      default:
+        return state
+    }
+  } 
+
+  const [taskList, dispatch] = useReducer(taskReducer, [])
+
+  const handleAdd = (e) => {
     e.preventDefault()
-    setListTask( list => list.concat(state))
+    dispatch({ type: 'add', payload: { ...state, id: uniqid(), isCompleted: false }})
     reset()
   }
 
+  const handleImportant = (id) => dispatch({ type: 'changeImportant', payload: id })
+
+  const handleDelete = (id) => dispatch({ type: 'delete', payload: id })
+
+  const handleComplete = (id) => dispatch({ type: 'complete', payload: id })
+
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form>
 
         <input
           type="text"
@@ -36,16 +67,18 @@ const ListView = () => {
           onClick={ (e) => handleChange(e, important) }
         >⭐</button>
         
-        <button>Guardar</button>
+        <button onClick={handleAdd}>Guardar</button>
 
       </form>
 
       <section>
         {
-          listTask.map( ({ task, important }) => 
-            <article>
-              <p>{task}</p>
-              <button>{important ? '⭐' : '😔'}</button>
+          taskList.map(({ task, important, id, isCompleted }) => 
+            <article key={id}>
+              <p style={isCompleted ? {textDecoration: 'line-through'} : null}>{task}</p>
+              <button onClick={() => handleImportant(id)}>{important ? 'imp' : 'not imp'}</button>
+              <button onClick={() => handleComplete(id)}>Hecho</button>
+              <button onClick={() => handleDelete(id)}>Borrar</button>
             </article>
           )
         }
